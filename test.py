54dbbb3564e8b745e6d18231228fef7da559ccac
roryclear <roryclear.rc@gmail.com>
2026-03-28 15:16:56 +0000

    show plt for now

--- test.py
+++ test.py
@@ -4,6 +4,44 @@ import cv2
 
 from blazeface import BlazeFace
 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def plot_detections(img, detections, with_keypoints=True):
+    fig, ax = plt.subplots(1, figsize=(10, 10))
+    ax.grid(False)
+    ax.imshow(img)
+    
+    if isinstance(detections, torch.Tensor):
+        detections = detections.cpu().numpy()
+
+    if detections.ndim == 1:
+        detections = np.expand_dims(detections, axis=0)
+
+    print("Found %d faces" % detections.shape[0])
+        
+    for i in range(detections.shape[0]):
+        ymin = detections[i, 0] * img.shape[0]
+        xmin = detections[i, 1] * img.shape[1]
+        ymax = detections[i, 2] * img.shape[0]
+        xmax = detections[i, 3] * img.shape[1]
+
+        rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                 linewidth=1, edgecolor="r", facecolor="none", 
+                                 alpha=detections[i, 16])
+        ax.add_patch(rect)
+
+        if with_keypoints:
+            for k in range(6):
+                kp_x = detections[i, 4 + k*2    ] * img.shape[1]
+                kp_y = detections[i, 4 + k*2 + 1] * img.shape[0]
+                circle = patches.Circle((kp_x, kp_y), radius=0.5, linewidth=1, 
+                                        edgecolor="lightskyblue", facecolor="none", 
+                                        alpha=detections[i, 16])
+                ax.add_patch(circle)
+        
+    plt.show()
+
 gpu = "cpu"
 
 front_net = BlazeFace().to(gpu)
@@ -23,3 +61,5 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 front_detections = front_net.predict_on_image(img)
 print(front_detections.shape)
 print(front_detections)
+
+plot_detections(img, front_detections)
